d9d47bc3111571a5e7c1cc3ed0eeb7f81b818a75
mijashgaunkuri <Mijashgc1012@gmail.com>
2026-07-01 22:21:26 +0545

    feat: add user model and migration setup

--- app/core/database.py
+++ app/core/database.py
@@ -1,7 +1,7 @@
-from sqlALchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from sqlALchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import declarative_base
 from app.core.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL,echo=True, future=True)
+engine = create_async_engine(settings.DATABASE_URL, echo=True, future=True)
 AsyncSessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 Base = declarative_base()
---
+++ app/models/user.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from sqlalchemy import Boolean, Integer, String, DateTime, func
+from sqlalchemy.orm import mapped_column,Mapped
+from app.core.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True, index=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, index=True, nullable=False)
+    full_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
